e18336b3e25cbf52a9d0eba51babf37f66c27c5a
Atharva-kul <atharvakulkarni2112005@gmail.com>
2026-02-03 19:20:40 +0530

    Update waves.py with 3D axes and fixed text

--- waves.py
+++ waves.py
@@ -1,9 +1,17 @@
 from manim import *
 import numpy as np
 
-class waves(Scene):
+class waves(ThreeDScene):
     def construct(self):
-        a= Axes(x_range=[-10, 10], y_range=[-5, 5])
+        
+        self.set_camera_orientation(phi=75 * DEGREES, theta=-145 * DEGREES, zoom=0.9)
+        
+        a= ThreeDAxes(x_range=[-10, 10], y_range=[-5, 5], z_range=[-5, 5], axis_config={"include_tip": True})
+        x_label = a.get_x_axis_label(Text("x")).scale(0.7)
+        y_label = a.get_y_axis_label(Text("y")).scale(0.7)
+        z_label = a.get_z_axis_label(Text("z")).scale(0.7)
+        self.add(x_label, y_label, z_label)
+        
         graph = a.plot(lambda x: np.sin(x)+np.cos(x/2)+np.sin(x*2)-np.cos(x*2),color=BLUE)
         
         s1= a.plot(lambda x: np.sin(x),color=RED)
@@ -13,15 +21,19 @@ class waves(Scene):
         
         g=VGroup(s1, s2, s3, s4)
         
-        text1 = Text("sin(x)+cos(x/2)+sin(x*2)-cos(x*2)", font_size=14)
-        text2 = Text("sin(x), cos(x/2), sin(x*2), -cos(x*2)", font_size=14)
-        self.add(text1.to_edge(UP))
+        text1 = Text("sin(x)+cos(x/2)+sin(x*2)-cos(x*2)", font_size=28)
+        text2 = Text("sin(x), cos(x/2), sin(x*2), -cos(x*2)", font_size=28)
+        
         self.add(graph, a)
-        self.add(text1.to_edge(UP))
+        self.add_fixed_in_frame_mobjects(text1.to_edge(UP, buff=0.5))
         self.wait(1)
-        self.play(ReplacementTransform(graph, g))
+        self.play(
+            ReplacementTransform(graph, g),
+            text1.animate.set_opacity(0),
+            run_time=2
+        )
         self.remove(text1)
-        self.add(text2.to_edge(DOWN))
+        self.add_fixed_in_frame_mobjects(text2.to_edge(DOWN, buff=0.5))
         
         self.wait(2)
         
